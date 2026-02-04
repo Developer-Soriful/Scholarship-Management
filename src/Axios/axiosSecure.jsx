6a38dc3f52893ef788dfa88bff1423a1a973514d
Developer-Soriful <93891396soriful@gmail.com>
2026-02-04 22:14:34 +0600

    added

--- src/Axios/axiosSecure.jsx
+++ src/Axios/axiosSecure.jsx
@@ -3,14 +3,13 @@ import axios from "axios";
 import { getIdToken } from "firebase/auth";
 
 const axiosSecure = axios.create({
-    baseURL: "https://server-side-12-1x95.onrender.com",
+    baseURL: "https://scholarship-management-backend-production.up.railway.app",
 });
 
 let interceptorId = null;
 
 // This function should be called from inside a component, passing the user
 export const attachAuthInterceptor = (user) => {
-    // Remove previous interceptor if exists
     if (interceptorId !== null) {
         axiosSecure.interceptors.request.eject(interceptorId);
     }
